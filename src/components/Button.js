5809c8cfba3868c7365c0a28b7d0558f861681d3
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 16:15:07 +0100

    feat: initial commit for src/components/Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ children, className, ...props }) => {
+  return (
+    <button className={`mui-button ${className}`} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
